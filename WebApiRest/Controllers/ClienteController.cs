2ec33c72c253312de6dc03c36eaee3b9494b1284
Sergio De Luca <sdlbsso@gmail.com>
2022-12-25 18:59:25 -0300

    controlador dotnet

---
+++ WebApiRest/Controllers/ClienteController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiRest.Model;
+
+namespace WebApiRest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClienteController :  ControllerBase
+    {
+
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarCliente()
+        {
+            List<Cliente> clientes = new List<Cliente>
+            {
+            new Cliente
+                {
+                    id = 1,
+                    nombre = "Paolo",
+                    edad = "25",
+                    correo = "lalola@mail.com"
+                },
+                new Cliente
+                {
+                    id = 2,
+                    nombre = "Zurdo",
+                    edad = "23",
+                    correo = "lazozo@mail.com"
+                }
+            };
+            return clientes;
+        }
+
+
+
+
+        [HttpGet]
+        [Route("cliente")]
+        public dynamic getCliente(int id)
+        {
+            return Ok();
+        }
+
+
+
+        [HttpPost]
+        [Route("guardar")]
+        public dynamic guardarCliente(Cliente cliente)
+        {
+            return Ok();
+        }
+
+
+        [HttpPost]
+        [Route("eliminar")]
+        public dynamic eliminarCliente(int id)
+        {
+            string token = Request.Headers.Where(x => x.Key == "Authorization").FirstOrDefault().Value;
+
+            if (token != "321321321")
+            {
+                return new
+                {
+                    success = false,
+                    message = "error con el token",
+                    result = id
+                };
+            }
+            return new
+            {
+                success = true,
+                message = "borrado exitoso",
+                result = id
+            };
+        }
+
+
+    }
+}
---
+++ WebApiRest/Model/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApiRest.Model
+{
+    public class Cliente
+    {
+
+
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string edad { get; set;}
+        public string correo { get; set; }
+
+
+    }
+}
